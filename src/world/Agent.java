9a690fae2b90c876d7082ddb4529b7f02f6e5f76
bassagap <bassagap@RTCMW0859M.emea.roche.com>
2016-10-17 19:59:05 +0200

    [17.10.2016] bassagap: new properties and constructor of Agent class

--- src/world/Agent.java
+++ src/world/Agent.java
@@ -10,5 +10,34 @@ package world;
  * @author u124308
  */
 public class Agent {
+    // Properties:
+    
+    private Vec2D speed;
+    private Vec2D maxSpeed;
+    private Vec2D pos;
+    private Vec2D dir;
+    private Vec2D obj;
+    private double radius;
+    private double weight;
+    private int id;
+    
+
+//Contructor
+    public Agent (int x, int y, double radius, double weight, int id){
+        this.speed = new Vec2D(x, y);
+        this.maxSpeed = new Vec2D(x, y);
+        this.pos = new Vec2D(x, y);
+        this.dir = new Vec2D(x, y);
+        this.obj = new Vec2D(x, y);
+        this.radius = radius;
+        this.weight = weight;
+        this.id = id;
+    }
+    // Methods:
+    public double getRadius() {
+        return this.radius;
+    }
+    
+    
     
 }
