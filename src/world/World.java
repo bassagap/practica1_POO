12a2388e8d30043f6eaf06b59b4bbc3b5e57351b
u124308 <u124308@54004p34.ds.upf.edu>
2016-10-17 20:01:47 +0200

    [17.10.2016] ivo: World class skeleton written

---
+++ src/world/World.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package world;
+import world.Agent;
+import world.Vec2D;
+/**
+ *
+ * @author u124308
+ */
+public class World {
+    private int width, height, screenMargin, numAgents;
+    Agent[] agents;
+    int getW() {
+    }
+    int getH() {
+    }
+    int getNumAgents() {
+        
+    }
+    Agent getAgent(){
+        
+    }
+    Vec2D randomPointInsideWorld() {
+        
+    }
+    void processCollisions(){
+        
+    }
+    void update() {
+        
+    }
+    void run(int steps) {
+        
+    }
+}
